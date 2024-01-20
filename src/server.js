246edb1eae55efb670a8dd53d2b5114ec130f450
Nhan Bui <buithiennhan0345@gmail.com>
2024-01-20 23:00:58 +0700

    second commit

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
-import express from 'express'
-import route from '~/routes'
-const app = express()
+import express from 'express';
+import route from '~/routes';
+const app = express();
 
-const hostname = 'localhost'
-const port = 3000
+const hostname = 'localhost';
+const port = 3000;
 
-route(app)
+route(app);
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at ${ hostname }:${ port }/`)
-})
+  console.log(`Server running at ${hostname}:${port}/`);
+});
